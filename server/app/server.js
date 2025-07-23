be97f56a30ef5b7b9c4c3ad925c9e25d82e198be
Prathamesh <prathameshjanugade2004@gmail.com>
2025-07-23 19:14:49 +0530

    added frontend and  nginx

--- server/app/server.js
+++ server/app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.send('<h1>Welcome to My Dockerized Node App</h1>');
 });
 
