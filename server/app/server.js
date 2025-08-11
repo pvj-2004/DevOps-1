0a689d130e6bcf692faa55242b75009f2660d8aa
Prathamesh <prathameshjanugade2004@gmail.com>
2025-08-11 13:07:08 +0530

    edited server.js

--- server/app/server.js
+++ server/app/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><p>Welcome to my Dockerized Node App!</p>');
+});
+
+// Hello route
 app.get('/api/hello', (req, res) => {
   res.send('<h1>Welcome to My Dockerized Node App</h1>');
 });
 
+// About route
 app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1><p>This is running inside Docker!</p>');
 });
